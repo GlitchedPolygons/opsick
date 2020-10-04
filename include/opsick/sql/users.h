e7db81f4230a2d9e15341c194d0def4115aea413
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-10-05 01:31:06 +0200

    Added more SQL "prepared statements" to sql/users.h

--- include/opsick/sql/users.h
+++ include/opsick/sql/users.h
@@ -34,8 +34,12 @@ static const char opsick_sql_get_user_pw_and_totps[] = "SELECT pw, totps FROM us
 static const char opsick_sql_set_user_pw[] = "UPDATE users SET pw = ?, lastmod_utc = (strftime('%s','now')) WHERE id = ?";
 static const char opsick_sql_set_user_totps[] = "UPDATE users SET totps = ?, lastmod_utc = (strftime('%s','now')) WHERE id = ?";
 
+static const char opsick_sql_get_user_iat[] = "SELECT iat_utc FROM users WHERE id = ?";
 static const char opsick_sql_get_user_exp[] = "SELECT exp_utc FROM users WHERE id = ?";
 static const char opsick_sql_set_user_exp[] = "UPDATE users SET exp_utc = ?, lastmod_utc = (strftime('%s','now')) WHERE id = ?";
+
+static const char opsick_sql_get_user_keys[] = "SELECT public_key_ed25519, encrypted_private_key_ed25519, public_key_curve448, encrypted_private_key_curve448 FROM users WHERE id = ?";
+static const char opsick_sql_set_user_keys[] = "UPDATE users SET public_key_ed25519 = ?, encrypted_private_key_ed25519 = ?, public_key_curve448 = ?, encrypted_private_key_curve448 = ?, lastmod_utc = (strftime('%s','now')) WHERE id = ?";
 
 #ifdef __cplusplus
 } // extern "C"
