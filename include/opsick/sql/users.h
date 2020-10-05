5b9d708ca7cabe66a7f6917d1a3d693af491320d
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-10-05 02:11:29 +0200

    More SQL stringzz

--- include/opsick/sql/users.h
+++ include/opsick/sql/users.h
@@ -27,6 +27,9 @@ extern "C" {
  * @brief SQL statements for interacting with the Opsick DB's users table (parametrized).
  */
 
+static const char opsick_sql_create_user[] = "INSERT INTO users (pw, exp_utc, body, public_key_ed25519, encrypted_private_key_ed25519, public_key_curve448, encrypted_private_key_curve448) VALUES (?, ?, ?, ?, ?, ?, ?);";
+static const char opsick_sql_delete_user[] = "DELETE FROM users WHERE id = ?";
+
 static const char opsick_sql_get_user_body[] = "SELECT body FROM users WHERE id = ?";
 static const char opsick_sql_set_user_body[] = "UPDATE users SET body = ?, lastmod_utc = (strftime('%s','now')) WHERE id = ?";
 
